2a111bcc0e5b1424d22c60b8c4a367701a84bad0
Dadoo <contact@david-libert.com>
2025-01-23 00:31:23 +0100

    tes elasticsearch mapping

--- mvp/elastic/test.py
+++ mvp/elastic/test.py
@@ -6,18 +6,13 @@ from pathlib import Path
 # Connexion au cluster
 es = Elasticsearch(hosts = "http://@localhost:9200")
 
-# Décommenter cette commande si vous utilisez l'installation sécurisée avec 3 nodes
-
-#es = Elasticsearch(hosts = "https://elastic:datascientest@localhost:9200",
-#                  ca_certs="./ca/ca.crt")
-
 def load_data():
 
     ROOT_DIR = Path(__file__).parent.parent
-    CAR_FILE = os.path.join(ROOT_DIR, "docs", "cars.csv")
+    CAR_FILE = os.path.join(ROOT_DIR, "docs", "EmployerReviews.csv")
     with open(CAR_FILE, encoding='utf-8') as f:
         reader = csv.DictReader(f)
-        helpers.bulk(es, reader, index='cars')
+        helpers.bulk(es, reader, index='reviews')
 
 
 if __name__ == "__main__":
