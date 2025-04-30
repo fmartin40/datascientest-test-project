47c34cf63e0e868f7d9efb21bc648b772245e2c5
Dadoo <contact@david-libert.com>
2025-05-01 00:15:52 +0200

    nettoyage config suppression environnement

--- celery/app/core/config.py
+++ celery/app/core/config.py
@@ -14,7 +14,7 @@ class Settings(BaseSettings):
 	
 	ENVIRONMENT: str = "local"  #  (local ou prod)
 
-	REDIS_HOST_LOCAL: str = "localhost"
+	REDIS_HOST: str 
 	REDIS_HOST_PROD: str 
 	REDIS_PORT: int 
 	REDIS_DB: int 
@@ -23,17 +23,17 @@ class Settings(BaseSettings):
 	POSTGRES_USER: str
 	POSTGRES_PASSWORD: str
 	POSTGRES_DB: str
-	POSTGRES_HOST_LOCAL: str 
+	POSTGRES_HOST: str 
 	POSTGRES_HOST_PROD: str 
 
 	# Elasticsearch (choix automatique selon l'environnement)
-	ELASTIC_HOST_LOCAL: str = "http://127.0.0.1:9200"
+	POSTGRES_HOST: str 
 	ELASTIC_HOST_PROD: str 
 	ELASTIC_INDEX: str = "jobs"
 
 	CELERY_RESULT_BACKEND: str = "rpc://"
 
-	RABBITMQ_URL_LOCAL: str = "localhost"
+	RABBITMQ_URL: str 
 	RABBITMQ_URL_PROD: str 
 	RABBITMQ_DEFAULT_USER: str
 	RABBITMQ_DEFAULT_PASS: str
@@ -44,22 +44,7 @@ class Settings(BaseSettings):
 	def SCRAP_API_TEST_RESULT_URL(self):
 		return f"http://api_scrap:{self.API_SCRAP_PORT}/profile/test/scraping-result"
 	
-	@property
-	def POSTGRES_HOST(self):
-		return self.POSTGRES_HOST_PROD if self.ENVIRONMENT == "prod" else self.POSTGRES_HOST_LOCAL
-
-	@property
-	def ELASTIC_HOST(self):
-		return self.ELASTIC_HOST_PROD if self.ENVIRONMENT == "prod" else self.ELASTIC_HOST_LOCAL
-
-	@property
-	def RABBITMQ_URL(self):
-		return self.RABBITMQ_URL_PROD if self.ENVIRONMENT == "prod" else self.RABBITMQ_URL_LOCAL
-
-	@property
-	def REDIS_HOST(self):
-		return self.REDIS_HOST_PROD if self.ENVIRONMENT == "prod" else self.REDIS_HOST_LOCAL
-
+	
 
 
 settings = Settings()  # type: ignore
